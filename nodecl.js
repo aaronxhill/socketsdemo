266d59ce64b2d517335c27f4b04c45747ec5929a
Aaron Hill <aaron.hill@me.com>
2015-12-13 07:08:00 -0500

    adding index.html

--- nodecl.js
+++
@@ -1,8 +0,0 @@
-var socket = require('socket.io-client')('https://ds-fall-15-aaronxhill.c9.io/');
-socket.on('connect', function(){
-	socket.emit('my other event', { from: 'client' });
-});
-socket.on('news', function(data){
-	console.log(data)
-});
-// socket.on('disconnect', function(){});
