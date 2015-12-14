ac1e2940cefcfead0524dc2c9c29259870d86f0e
Aaron Hill <aaron.hill@me.com>
2015-12-14 10:16:38 -0500

    incorporating pg query into app

--- app.js
+++ app.js
@@ -5,37 +5,53 @@ var data3 = fs.readFileSync(__dirname + '/index3.html');
 
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
+var pg = require('pg');
+
+var conString = "postgres://aaron:kitty555@data-structures.c2g7qw1juwkg.us-east-1.rds.amazonaws.com:5432/postgres";
 
 app.listen(8080);
 
-// function handler (req, res) {
-//   fs.readFile(__dirname + '/index.html',
-//   function (err, data) {
-//     if (err) {
-//       res.writeHead(500);
-//       return res.end('Error loading index.html');
-//     }
-
-//     res.writeHead(200);
-//     res.end(data);
-//   });
-// }
-
 function handler (req, res) {
-  // fs.readFile(__dirname + '/index.html',
-  // function (err, data) {
-  //   if (err) {
-  //     res.writeHead(500);
-  //     return res.end('Error loading index.html');
-  //   }
 
-    res.writeHead(200);
-    res.write(data1);
-    res.write(data2);
-    res.end(data3);
-  // });
+    pg.connect(conString, function(err, client, done) {
+
+        var handleError = function(err) {
+            // no error occurred, continue with the request
+            if (!err) return false;
+
+            // An error occurred, remove the client from the connection pool.
+            // A truthy value passed to done will remove the connection from the pool
+            // instead of simply returning it to be reused.
+            // In this case, if we have successfully received a client (truthy)
+            // then it will be removed from the pool.
+            if (client) {
+                done(client);
+            }
+            res.writeHead(500, {'content-type': 'text/plain'});
+            res.end('An error occurred');
+            return true;
+        };
+
+        // handle an error from the connection
+        if (handleError(err)) return;
+
+        // get the total number of visits today (including the current visit)
+        client.query('SELECT * FROM buttondata;', function(err, result) {
+
+            // handle an error from the query
+            if (handleError(err)) return;
+
+            // return the client to the connection pool for other requests to reuse
+            done();
+            res.writeHead(200, {'content-type': 'text/html'});
+            res.write(data1);
+            res.write('var dataset = ' + JSON.stringify(result.rows) + ';');
+            res.end(data3);
+            res.end();
+        });
+    });
+    
 }
-
 
 io.on('connection', function (socket) {
   // socket.emit('news', { hello: 'world' });
