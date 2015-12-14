cf27e71577440436c1167087ccd3d19065a1b849
Aaron Hill <aaron.hill@me.com>
2015-12-14 10:05:21 -0500

    index into three parts

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
+var fs = require('fs');
+var data1 = fs.readFileSync(__dirname + '/index1.html');
+var data2 = fs.readFileSync(__dirname + '/index2.html');
+var data3 = fs.readFileSync(__dirname + '/index3.html');
+
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
-var fs = require('fs');
 
 app.listen(8080);
 
+// function handler (req, res) {
+//   fs.readFile(__dirname + '/index.html',
+//   function (err, data) {
+//     if (err) {
+//       res.writeHead(500);
+//       return res.end('Error loading index.html');
+//     }
+
+//     res.writeHead(200);
+//     res.end(data);
+//   });
+// }
+
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+  // fs.readFile(__dirname + '/index.html',
+  // function (err, data) {
+  //   if (err) {
+  //     res.writeHead(500);
+  //     return res.end('Error loading index.html');
+  //   }
 
     res.writeHead(200);
-    res.end(data);
+    res.write(data1);
+    res.write(data2);
+    res.end(data3);
   });
 }
+
 
 io.on('connection', function (socket) {
   // socket.emit('news', { hello: 'world' });
