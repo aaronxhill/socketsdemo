4eda74bde856f0a62ee89e9cf2da86fe40b86192
Aaron Hill <aaron.hill@me.com>
2015-12-14 10:53:08 -0500

    removed data2 var from app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var data1 = fs.readFileSync(__dirname + '/index1.html');
-var data2 = fs.readFileSync(__dirname + '/index2.html');
+// var data2 = fs.readFileSync(__dirname + '/index2.html');
 var data3 = fs.readFileSync(__dirname + '/index3.html');
 
 var app = require('http').createServer(handler)
@@ -36,7 +36,7 @@ function handler (req, res) {
         if (handleError(err)) return;
 
         // get the total number of visits today (including the current visit)
-        client.query('SELECT * FROM buttondata;', function(err, result) {
+        client.query('SELECT * FROM buttondata2;', function(err, result) {
 
             // handle an error from the query
             if (handleError(err)) return;
